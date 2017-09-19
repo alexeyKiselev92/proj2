31c750713315e24a3bf6eaedbdb7004ad6dd3e11
alexeyKiselev92 <31734433+alexeyKiselev92@users.noreply.github.com>
2017-09-19 17:17:10 +0300

    fix #6
    Move getting data to MotionComponent

--- src/app/myComponents/motionsComponent/motionComponent.ts
+++ src/app/myComponents/motionsComponent/motionComponent.ts
@@ -1,15 +1,49 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { MotionTable } from '../motionsTable/motionTable';
 import { TableRowItem } from '../../Types/table-row-item';
+import { DataService } from '../../service/data.service';
 
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'motion-component',
     templateUrl: './motionComponent.html',
-    styleUrls: ['./motionComponent.css']
+    styleUrls: ['./motionComponent.css'],
+    providers: [DataService]
 })
 export class MotionComponent {
     selected: TableRowItem;
+    items: TableRowItem[];
+    itemsJSON: TableRowItem[];
+
+    constructor(private dataService: DataService) {
+
+    }
+
+    // tslint:disable-next-line:use-life-cycle-interface
+    ngOnInit() {
+        this.Update();
+    }
+
+    Update(): void {
+        this.dataService.getTableRowItemsObservable().subscribe(data => this.items = this.orderByDesc(data).slice(0, 3));
+        this.dataService.getTableRowItemsJSON().subscribe(data => this.itemsJSON = this.orderByDesc(data).slice(0, 3));
+    }
+
+    orderByDesc(data: TableRowItem[]): TableRowItem[] {
+        return data.sort((a: TableRowItem, b: TableRowItem) => {
+            if (a.value > b.value) {
+                return -1;
+            }
+            // tslint:disable-next-line:one-line
+            else if (a.value < b.value) {
+                return 1;
+            }
+            // tslint:disable-next-line:one-line
+            else {
+                return 0;
+            }
+        });
+    }
 
     onSelectedItem(item: TableRowItem): void {
         this.selected = item;
--- src/app/myComponents/motionsTable/motionTable.ts
+++ src/app/myComponents/motionsTable/motionTable.ts
@@ -8,49 +8,16 @@ import { OrderByDesc } from '../../myPipes/descPipe';
     // tslint:disable-next-line:component-selector
     selector: 'motiontable',
     templateUrl: './motionTable.html',
-    styleUrls: ['./motionTable.css'],
-    providers: [DataService]
+    styleUrls: ['./motionTable.css']
 })
 // tslint:disable-next-line:component-class-suffix
-export class MotionTable implements OnInit {
-    @Input() fromJSON: boolean;
+export class MotionTable {
     @Input() tableName: string;
     @Output() selectedItem: EventEmitter<TableRowItem> = new EventEmitter<TableRowItem>();
-    items: TableRowItem[];
+    @Input()items: TableRowItem[];
 
-    constructor(private dataService: DataService) {
+    constructor() {
 
-    }
-
-    Update(fromJSON: boolean): void {
-        // tslint:disable-next-line:curly
-        if (!fromJSON) {
-            this.dataService.getTableRowItemsObservable().subscribe(data => this.items = this.orderByDesc(data).slice(0, 3));
-        }
-        // tslint:disable-next-line:curly
-        // tslint:disable-next-line:one-line
-        else {
-            this.dataService.getTableRowItemsJSON().subscribe(data => this.items = this.orderByDesc(data).slice(0, 3));
-        }
-    }
-
-
-    ngOnInit() {
-        this.Update(this.fromJSON);
-    }
-
-    orderByDesc(data: TableRowItem[]): TableRowItem[]{
-        return data.sort((a: TableRowItem, b: TableRowItem) =>{
-            if (a.value > b.value){
-                return -1;
-            }
-            else if (a.value < b.value){
-                return 1;
-            }
-            else{
-                return 0;
-            }
-        });
     }
 
     onMouseHover(item: TableRowItem) {
